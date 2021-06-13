f38d8f207fc313d7b4debc55b3ae08a1c70d7a0c
Daniel D <danieldobosp@gmail.com>
2021-06-13 14:45:53 -0500

    Simulate multiple bodies

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+function GravityBitch(x, y, v_x, v_y, m) {
+    this.pos = {
+        x: x,
+        y: y,
+    };
+    this.vel = {
+        x: v_x,
+        y: v_y,
+    };
+    this.acc = {
+        x: 0,
+        y: 0,
+    };
+    this.m = m;
+}
+
+function Ship(x, y, m, r, i) {
+    this.pos.x = x;
+    this.pos.y = y;
+    this.vel.x = 0;
+    this.vel.y = 0;
+    this.m = m;
+    this.rot = r;
+    this.thrust = false;
+    this.impulse = i;
+}
+
+const shipProto = new GravityBitch();
+shipProto.rot = 0;
+shipProto.thrust = false;
+shipProto.impulse = 0;
+Ship.prototype = shipProto;
+
 (function() {
     const canvas = document.getElementById("game");
     const instructions = document.getElementById("instructions");
@@ -7,7 +40,6 @@
     const context = canvas.getContext("2d");
 
     const keysDown = {};
-
     document.body.addEventListener(
         "keydown",
         function(e) {
@@ -22,33 +54,16 @@
         }
     );
 
-    const ship = {
-        pos: {
-            x: 100, // m
-            y: 100, // m
-        },
-        vel: {
-            x: 0, // m/s
-            y: 0, // m/s
-        },
-        acc: {
-            x: 0,
-            y: 0,
-        },
-        rot: Math.PI / 2,
-        thrust: false,
-        impulse: 16,
-    };
+    const G = 6.674 * Math.pow(10, -11);
+
+    const ship = new Ship(50, 100, 1000, 0, 16);
 
     const objects = [
         ship,
+        new GravityBitch(748, 442, 19, 62, 1000),
+        new GravityBitch(875, 688, -50, -11, Math.pow(10, 14)),
+        new GravityBitch(canvas.width / 2, canvas.height / 2, 0, 0, Math.pow(10, 16)),
     ];
-
-    const gw = {
-        x: canvas.width / 2,
-        y: canvas.height / 2,
-        g: 9.81,
-    };
 
     const second = 1000; // ms
     let t = Date.now();
@@ -67,71 +82,81 @@
         // ship under thrust?
         ship.thrust = keysDown["w"];
 
-        simulate(h);
+        for (let o = 0; o < objects.length; o++) {
+            simulate(objects[o], h);
+        }
         render();
         t = now;
         window.requestAnimationFrame(loop);
     }
 
-    function simulate(h) {
-        const [x_gw, y_gw] = unit([gw.x - ship.pos.x, gw.y - ship.pos.y]);
-        ship.acc.x = x_gw * gw.g;
-        ship.acc.y = y_gw * gw.g;
-        if (ship.thrust) {
+    function simulate(o, h) {
+        // add up forces subjected by other objects
+        o.acc.x = 0;
+        o.acc.y = 0;
+        for (let i_o = 0; i_o < objects.length; i_o++) {
+            const o_ = objects[i_o];
+            if (o != o_) {
+                const dir_o_ = [o_.pos.x - o.pos.x, o_.pos.y - o.pos.y];
+                const [x_o_, y_o_] = unit(dir_o_);
+                const norm_o_ = norm(dir_o_);
+                const f = G * (o.m * o_.m) / Math.pow(norm_o_, 2);
+                const a = f / o.m;
+                o.acc.x += x_o_ * a;
+                o.acc.y += y_o_ * a;
+            }
+        }
+        if (o.thrust) {
             const dir_rot = [
-                Math.cos(ship.rot),
-                Math.sin(ship.rot),
+                Math.cos(o.rot),
+                Math.sin(o.rot),
             ];
             const [x_r, y_r] = unit(dir_rot);
-            ship.acc.x += x_r * ship.impulse;
-            ship.acc.y += y_r * ship.impulse;
+            o.acc.x += x_r * o.impulse;
+            o.acc.y += y_r * o.impulse;
         }
-        ship.vel.x += h * ship.acc.x;
-        ship.vel.y += h * ship.acc.y;
-        ship.pos.x += h * ship.vel.x;
-        ship.pos.y += h * ship.vel.y;
+        o.vel.x += h * o.acc.x;
+        o.vel.y += h * o.acc.y;
+        o.pos.x += h * o.vel.x;
+        o.pos.y += h * o.vel.y;
     }
 
     function render() {
-        //     console.log(`${(t - start) / second}\t${ship.pos.x}\t${ship.pos.y}`)
-
         // blank screen
         context.fillStyle = "white";
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.fillStyle = "black";
-
-        // ship
-        context.beginPath();
-        context.strokeStyle = "black";
-        context.ellipse(ship.pos.x, ship.pos.y, 3, 3, 0, 0, 2 * Math.PI);
-        context.stroke();
-
-        // planet
-        context.beginPath();
-        context.ellipse(gw.x, gw.y, 20, 20, 0, 0, 2 * Math.PI);
-        context.stroke();
-
-        // acceleration vector indicator
-        const dir_gw = [ship.acc.x, ship.acc.y];
-        const size_acc = norm([ship.acc.x, ship.acc.y]);
-        const [x_, y_] = unit(dir_gw);
-        context.beginPath();
-        context.strokeStyle = "red";
-        context.moveTo(ship.pos.x + x_ * 5, ship.pos.y + y_ * 5);
-        context.lineTo((ship.pos.x + x_ * size_acc), (ship.pos.y + y_ * size_acc));
-        context.stroke();
-
-        // ship rotation indicator
-        const dir_rot = [
-            Math.cos(ship.rot),
-            Math.sin(ship.rot)
-        ];
-        const [x_r, y_r] = unit(dir_rot);
-        context.beginPath();
-        context.strokeStyle = "green";
-        context.moveTo(ship.pos.x + x_r * 5, ship.pos.y + y_r * 5);
-        context.lineTo((ship.pos.x + x_r * 20), (ship.pos.y + y_r * 20));
-        context.stroke();
+        // objects
+        for (let i_o = 0; i_o < objects.length; i_o++) {
+            const o = objects[i_o];
+            context.beginPath();
+            context.strokeStyle = "black";
+            const r = o.m > 1000000 ? 20 : 3;
+            context.ellipse(o.pos.x, o.pos.y, r, r, 0, 0, 2 * Math.PI);
+            context.stroke();
+            // acceleration vector indicator
+            const dir_gw = [o.acc.x, o.acc.y];
+            const size_acc = norm([o.acc.x, o.acc.y]);
+            const [x_, y_] = unit(dir_gw);
+            context.beginPath();
+            context.strokeStyle = "red";
+            context.moveTo(o.pos.x + x_ * 5, o.pos.y + y_ * 5);
+            context.lineTo((o.pos.x + x_ * size_acc), (o.pos.y + y_ * size_acc));
+            context.stroke();
+            // ship rotation indicator
+            if (o instanceof Ship) {
+                const dir_rot = [
+                    Math.cos(o.rot),
+                    Math.sin(o.rot)
+                ];
+                const [x_r, y_r] = unit(dir_rot);
+                context.beginPath();
+                context.strokeStyle = "green";
+                context.moveTo(o.pos.x + x_r * 5, o.pos.y + y_r * 5);
+                context.lineTo((o.pos.x + x_r * 20), (o.pos.y + y_r * 20));
+                context.stroke();
+            }
+        }
     }
 
     window.requestAnimationFrame(loop);
